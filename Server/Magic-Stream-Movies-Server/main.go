8ac690e38eea3139fd14e276752951c11280fff2
Advait N Khandale <advait@Mac.lan>
2026-07-22 19:12:31 -0400

    Add get movie API

---
+++ Server/Magic-Stream-Movies-Server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	controller "github.com/Advait2207/Magic-Stream-Movies/Server/Magic-Stream-Movies-Server/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies..!")
+	})
+
+	router.GET("/movies", controller.GetMovies())
+	router.GET("/movie/:imdb_id", controller.GetMovie())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+}
