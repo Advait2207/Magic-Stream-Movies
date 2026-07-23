9e6a6dcc1352eb1df49f1b7c14281198fc5b62b9
Advait N Khandale <advait@Mac.lan>
2026-07-22 21:36:06 -0400

    add movie POST API functionality added

--- Server/Magic-Stream-Movies-Server/controllers/movie_controller.go
+++ Server/Magic-Stream-Movies-Server/controllers/movie_controller.go
@@ -8,11 +8,14 @@ import (
 	"github.com/Advait2207/Magic-Stream-Movies/Server/Magic-Stream-Movies-Server/database"
 	"github.com/Advait2207/Magic-Stream-Movies/Server/Magic-Stream-Movies-Server/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -45,7 +48,7 @@ func GetMovie() gin.HandlerFunc {
 
 		movieID := c.Param("imdb_id")
 
-		if movieID == ""{
+		if movieID == "" {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
 			return
 		}
@@ -55,10 +58,38 @@ func GetMovie() gin.HandlerFunc {
 		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieID}).Decode(&movie)
 
 		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error":"Movie not found"})
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
 			return
 		}
 
 		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed", "details": err.Error()})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add movie"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
 	}
 }
--- Server/Magic-Stream-Movies-Server/models/movie_model.go
+++ Server/Magic-Stream-Movies-Server/models/movie_model.go
@@ -7,36 +7,36 @@ import (
 
 type Genre struct {
 	// uniquely identify genres within the database
-	GenreID int `bson:"genre_id" json: "genre_id" validate:"required"`
+	GenreID int `bson:"genre_id" json:"genre_id" validate:"required"`
 
 	// actual genre names
-	GenreName string `bson:"genre_name" json: "genre_name" validate:"required, min=2, max=100"`
+	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
 }
 
 type Ranking struct {
-	RankingValue int `bson:"ranking_value" json: "ranking_value" validate:"required"`
+	RankingValue int `bson:"ranking_value" json:"ranking_value" validate:"required"`
 
-	RankingName string `bson:"ranking_name" json: "ranking_name" validate:"required"`
+	RankingName string `bson:"ranking_name" json:"ranking_name" validate:"required"`
 }
 
 type Movie struct {
-	ID bson.ObjectID `bson:"_id" json: "_id"`
+	ID bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 
 	// unique identifier assigned by internet movie database
-	ImdbID string `bson:"imdb_id" json: "imdb_id" validate:"required"`
+	ImdbID string `bson:"imdb_id" json:"imdb_id" validate:"required"`
 
-	Title string `bson:"title" json: "title" validate:"required, min=2, max=500"`
+	Title string `bson:"title" json:"title" validate:"required,min=2,max=500"`
 
 	// store url of publically available poster image of the movie
-	PosterPath string `bson:"poster_path" json: "poster_path" validate:"required,url"`
+	PosterPath string `bson:"poster_path" json:"poster_path" validate:"required,url"`
 
 	// trailer of the movie, steam this video instead of streaming an actual movie
-	YouTubeID string `bson:"youtube_id" json: "youtube_id" validate:"required"`
+	YouTubeID string `bson:"youtube_id" json:"youtube_id" validate:"required"`
 
-	Genre []Genre `bson:"genre" json: "genre" validate:"required,dive"`
+	Genre []Genre `bson:"genre" json:"genre" validate:"required,dive"`
 
 	// store sentiments of movie reviews
-	AdminReview string `bson:"admin_review" json: "admin_review" validate:"required"`
+	AdminReview string `bson:"admin_review" json:"admin_review" validate:"required"`
 
-	Ranking Ranking `bson:"ranking" json: "ranking" validate:"required"`
+	Ranking Ranking `bson:"ranking" json:"ranking" validate:"required"`
 }
