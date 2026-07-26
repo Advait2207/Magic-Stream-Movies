39fb895f2b107cbffa7cac2a24b62f5672f80678
Advait Khandale <59452681+Advait2207@users.noreply.github.com>
2026-07-26 02:06:25 -0400

    Implement authorization middleware for protected API endpoints
    Closes #1
    
    - Added JWT authorization middleware
    - Configured protected and public routes
    - Validated authorization tokens
    - Stored authenticated user information in the Gin context
    - Added authorization checks for protected endpoints

---
+++ Server/Magic-Stream-Movies-Server/routes/unprotected_routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	controller "github.com/Advait2207/Magic-Stream-Movies/Server/Magic-Stream-Movies-Server/controllers"
+	"github.com/gin-gonic/gin"	
+)
+
+func SetupUnProtectedRoutes(router *gin.Engine) {
+	router.GET("/movies", controller.GetMovies())
+	router.POST("/register", controller.RegisterUser())
+	router.POST("/login", controller.LoginUser())
+}
