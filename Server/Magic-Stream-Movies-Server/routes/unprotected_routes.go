b205794f131deb46009f7af97a54f6b706a4e751
Advait N Khandale <advait@Mac.lan>
2026-07-28 00:18:10 -0400

    Code AdminReview update API

--- Server/Magic-Stream-Movies-Server/routes/unprotected_routes.go
+++ Server/Magic-Stream-Movies-Server/routes/unprotected_routes.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	controller "github.com/Advait2207/Magic-Stream-Movies/Server/Magic-Stream-Movies-Server/controllers"
-	"github.com/gin-gonic/gin"	
+	"github.com/gin-gonic/gin"
 )
 
 func SetupUnProtectedRoutes(router *gin.Engine) {
 	router.GET("/movies", controller.GetMovies())
 	router.POST("/register", controller.RegisterUser())
 	router.POST("/login", controller.LoginUser())
+	router.PATCH("/updatereview/:imdb_id", controller.AdminReviewUpdate())
 }
