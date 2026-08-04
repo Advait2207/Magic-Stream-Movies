be36e5328c32734bdf4a9edc522a283398366ead
Advait N Khandale <advait@Advaits-MacBook-Air-4.local>
2026-08-03 21:13:16 -0400

    fix: remove duplicate Home component rendering

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         <Route path = "/register" element={<Register/>}></Route>
         <Route path = "/login" element={<Login/>}></Route>
       </Routes>
-      <Home/>
     </>
   )
 }
