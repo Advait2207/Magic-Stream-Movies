68cf60d9f6e2b0cf93e2a3755e0860fe5005b577
Advait N Khandale <advait@Advaits-MacBook-Air-4.local>
2026-08-03 20:47:51 -0400

    Header component added

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
       <Home/>
     </>
   )
