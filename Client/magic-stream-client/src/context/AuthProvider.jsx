0641901a601acbbaa37e077e8d54413d880e2d55
Advait N Khandale <advait@Advaits-MacBook-Air-4.local>
2026-08-09 19:33:48 -0400

    Modify Auth Provider to get user data from the local storage

--- Client/magic-stream-client/src/context/AuthProvider.jsx
+++ Client/magic-stream-client/src/context/AuthProvider.jsx
@@ -1,14 +1,35 @@
-import {createContext, useState} from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext({});
 
-export const AuthProvider = ({children}) => {
-    const [auth, setAuth] = useState();
+export const AuthProvider = ({ children }) => {
+
+    const [auth, setAuth] = useState(() => {
+        const storedUser = localStorage.getItem('user');
+
+        if (!storedUser) return null;
+
+        try {
+            return JSON.parse(storedUser);
+        } catch (error) {
+            console.error('Failed to parse user from localStorage', error);
+            return null;
+        }
+    });
+
+    useEffect(() => {
+        if (auth) {
+            localStorage.setItem('user', JSON.stringify(auth));
+        } else {
+            localStorage.removeItem('user');
+        }
+    }, [auth]);
+
     return (
-        <AuthContext.Provider value = {{auth,setAuth}}>
+        <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export default AuthContext;
