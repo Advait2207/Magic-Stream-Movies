b6118d0bf71e41f7ae124261c7d5f44db89d7e71
Advait Khandale <59452681+Advait2207@users.noreply.github.com>
2026-08-10 17:22:30 -0400

    feat: implement movie trailer streaming
    - Add movie trailer streaming using embedded YouTube videos
    - Add application branding to the header and authentication pages
    - Add interactive hover effects to movie posters
    - Add play icon overlay for movie trailers

--- Client/magic-stream-client/src/components/movie/Movie.jsx
+++ Client/magic-stream-client/src/components/movie/Movie.jsx
@@ -1,10 +1,18 @@
 import Button from 'react-bootstrap/Button'
-
+import {Link} from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCirclePlay} from '@fortawesome/free-solid-svg-icons';
+import "./Movie.css"
 
 const Movie = ({movie, updateMovieReview}) => {
     return (
-        <div className="col-md-4 mb-4">
-            <div className="card h-100 shadow-sm">
+        <div className="col-md-4 mb-4" key={movie._id}>
+            <Link 
+                to={`/stream/${movie.youtube_id}`}
+                style={{textDecoration: 'none', color: 'inherit'}}
+            >
+
+            <div className="card h-100 shadow-sm movie-card">
                 <div style={{position:"relative"}}>
                     <img src={movie.poster_path} alt={movie.title}
                         className="card-img-top"
@@ -14,6 +22,10 @@ const Movie = ({movie, updateMovieReview}) => {
                             width: "100%"
                         }}
                     />
+
+                    <span className="play-icon-overlay">
+                        <FontAwesomeIcon icon={faCirclePlay} />
+                    </span>
 
                 </div>
                 <div className="card-body d-flex flex-column">
@@ -38,7 +50,7 @@ const Movie = ({movie, updateMovieReview}) => {
                     </Button>
                 )}
             </div>
-
+            </Link>
         </div>
     )
 }
