1d0b855facb9c1c1391357d6422d0e9c0920d368
Advait Khandale <59452681+Advait2207@users.noreply.github.com>
2026-08-10 00:32:06 -0400

    feat: persist authentication across page refresh
    - Modify Auth Provider to get user data from the local storage
    - feat: restore authentication state during app initialization
    - feat: update authentication persistence flow
    - style: improve authenticated user display in header

--- Client/magic-stream-client/src/context/AuthProvider.jsx
+++ Client/magic-stream-client/src/context/AuthProvider.jsx
@@ -1,14 +1,38 @@
-import {createContext, useState} from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext({});
 
-export const AuthProvider = ({children}) => {
-    const [auth, setAuth] = useState();
+export const AuthProvider = ({ children }) => {
+
+    const [loading, setLoading] = useState(true);
+    const [auth, setAuth] = useState(() => {
+        try {
+            const storedUser = localStorage.getItem('user');
+
+            if (!storedUser) return null;
+       
+            return JSON.parse(storedUser);
+        } catch (error) {
+            console.error('Failed to parse user from localStorage', error);
+            return null;
+        } finally {
+            setLoading(false);
+        }
+    });
+
+    useEffect(() => {
+        if (auth) {
+            localStorage.setItem('user', JSON.stringify(auth));
+        } else {
+            localStorage.removeItem('user');
+        }
+    }, [auth]);
+
     return (
-        <AuthContext.Provider value = {{auth,setAuth}}>
+        <AuthContext.Provider value={{ auth, setAuth, loading }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export default AuthContext;
